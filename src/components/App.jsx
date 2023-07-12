ecd9e121c1636fdd7bf3fb7431164dcf48d15bac
AndriySachyk <asachyk1312@gmail.com>
2023-07-12 20:37:55 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,25 @@ import {Filter} from './FilterContact/FilterContact';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    contacts: [ { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
     filter: '',
   };
 
+
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem('contacts')
+    if(localContacts) this.setState({contacts: JSON.parse(localContacts)})
+}
+
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts)
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  }
   createContact = data => {
     const newContact = { ...data, id: nanoid() };
     console.log(newContact);
